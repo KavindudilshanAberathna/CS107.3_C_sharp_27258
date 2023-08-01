dee9f869a745c4faef07e76925def8f55b11eff0
KavindudilshanAberathna <135214823+KavindudilshanAberathna@users.noreply.github.com>
2023-08-01 07:16:13 +0530

    Add files via upload

---
+++ Lab 06/Question1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            private void btnNavigateToForm2_Click(object sender, EventArgs e)
+            {
+                // Create an instance of the second form
+                Form2 secondForm = new Form2();
+
+                // Show the second form
+                secondForm.Show();
+            }
+
+        }
+    }
+}
