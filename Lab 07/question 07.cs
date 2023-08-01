88fc88295a5b5d15da3f97dc67e9d1a2ad84c726
KavindudilshanAberathna <135214823+KavindudilshanAberathna@users.noreply.github.com>
2023-08-01 07:16:35 +0530

    Add files via upload

---
+++ Lab 07/question 07.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        {
+             void Main()
+            {
+                Console.Write("Enter the size of the arrays: ");
+                int size = int.Parse(Console.ReadLine());
+
+                int[] array1 = new int[size];
+                int[] array2 = new int[size];
+                int[] resultArray = new int[size];
+
+                // Input values for array1
+                Console.WriteLine("Enter values for array1:");
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write($"Element {i}: ");
+                    array1[i] = int.Parse(Console.ReadLine());
+                }
+
+                // Input values for array2
+                Console.WriteLine("Enter values for array2:");
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write($"Element {i}: ");
+                    array2[i] = int.Parse(Console.ReadLine());
+                }
+
+                // Scalar Sum
+                int scalarSum = 0;
+                for (int i = 0; i < size; i++)
+                {
+                    scalarSum += array1[i];
+                }
+                Console.WriteLine("Scalar Sum: " + scalarSum);
+
+                // Vector Sum
+                for (int i = 0; i < size; i++)
+                {
+                    resultArray[i] = array1[i] + array2[i];
+                }
+                Console.WriteLine("Vector Sum: " + string.Join(", ", resultArray));
+
+                // Vector Product
+                for (int i = 0; i < size; i++)
+                {
+                    resultArray[i] = array1[i] * array2[i];
+                }
+                Console.WriteLine("Vector Product: " + string.Join(", ", resultArray));
+
+                // Scalar Product
+                int scalarProduct = 1;
+                for (int i = 0; i < size; i++)
+                {
+                    resultArray[i] = array1[i] * array2[i];
+                    scalarProduct *= resultArray[i];
+                }
+                Console.WriteLine("Scalar Product: " + scalarProduct);
+            }
+        }
+
+    }
+}
+}
---
+++ Lab 07/question 08.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Animal
+        {
+            public void Display()
+            {
+                Console.WriteLine("I am an Animal.");
+            }
+        }
+
+        class Dog : Animal
+        {
+            public void DisplayDog()
+            {
+                Console.WriteLine("I have four legs.");
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                Animal animal = new Animal();
+                Dog dog = new Dog();
+
+                animal.Display();
+                dog.DisplayDog();
+            }
+        }
+
+    
+
+
